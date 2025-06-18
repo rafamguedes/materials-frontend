3f139b8d26df885b5fe7236de2f862de7629fac3
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-06-18 09:11:43 -0300

    refact(TASK-12): Header component refactored

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,20 @@
 import './Header.css';
+import logo from '../../assets/logo.png';
 
 const Header = () => {
   return (
     <header className="menu-container">
       <div className="menu-content">
         <div className="menu-logo">
-            <img src="./src/assets/logo.png" alt="Logo" id="logo-img" />
-            <span className="logo-text">KIPFLOW</span>
+          <img src={logo} alt="Logo" id="logo-img" />
+          <span className="logo-text">KIPFLOW</span>
         </div>
 
         <nav className="menu-items">
           <a href="/" className="menu-item">Reservas</a>
           <a href="/items" className="menu-item">Equipamentos</a>
           <a href="/users" className="menu-item">Usuários</a>
-          <a href="/reports" className="menu-item">Relatórios</a>
-          <a href="/perfil" className="menu-item">Perfil</a>
+          <a href="/reports/reservations" className="menu-item">Relatórios</a>
         </nav>
       </div>
     </header>
